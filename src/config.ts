6290acdc7a3e7954cfba9878bd7e30c225dbd561
张晓伟 <zhangxiaowei.chris@bytedance.com>
2020-09-02 11:21:02 +0800

    chore: 修改为本地域名

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
-  server: process.env.NODE_ENV === 'production' ? 'https://api.overfronted.net' : 'http://localhost:5000',
+  server: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000',
   cdn: '/static/image'
 }
